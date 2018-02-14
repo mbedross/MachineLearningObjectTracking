cd878cb6b98e34c31638b2453c571f5b6cfad35f
Manu <mbedross@caltech.edu>
2018-02-14 13:36:54 -0800

    Removed gpuArray function from findDuplicates.m

--- preProcessing/findDuplicates.m
+++ preProcessing/findDuplicates.m
@@ -5,8 +5,8 @@ NF = length(holoDir(not([holoDir.isdir])));  % number of holograms
 
 dupes = zeros(NF,1);
 for k = 1 : NF-1
-    I1 = gpuArray(imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k-1))));
-    I2 = gpuArray(imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k))));
+    I1 = imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k-1)));
+    I2 = imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k)));
     diff = I1-I2;
     if range(diff)==0
         dupes(k) = 1;
