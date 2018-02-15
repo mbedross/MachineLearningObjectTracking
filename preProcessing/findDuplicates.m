e62f29b95fc4a878e87c83d3b3791340680aabe8
Manu <mbedross@caltech.edu>
2018-02-15 09:01:59 -0800

    General Bug Fixes. Also adjusted GUI size settings to not cut off text

--- preProcessing/findDuplicates.m
+++ preProcessing/findDuplicates.m
@@ -4,11 +4,11 @@ holoDir = dir(fullfile(masterDir,'Holograms'));
 NF = length(holoDir(not([holoDir.isdir])));  % number of holograms
 
 dupes = zeros(NF,1);
-for k = 1 : NF-1
-    I1 = imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k-1)));
-    I2 = imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k)));
+parfor k = 1 : NF-1
+    I1 = gpuArray(imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k-1))));
+    I2 = gpuArray(imread(fullfile(masterDir, 'Holograms', sprintf('%05d_holo.tif', k))));
     diff = I1-I2;
-    if range(diff)==0
+    if range(gather(diff))==0
         dupes(k) = 1;
     end
 end
--- preProcessing/preProcessingMain.m
+++ preProcessing/preProcessingMain.m
@@ -6,10 +6,15 @@ function [times, zSorted] = preProcessingMain(innerRadius, outerRadius, centerX,
 % getting it ready for ML tracking.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+tic
+% Turn duplicate directory warning off
+w = warning('query','last');
+id = w.identifier;
+warning('off',id)
 
 global n type masterDir batchSize
 N = n;
+bSize = batchSize;
 meanDir = fullfile(masterDir, 'MeanStack');
 mkdir(meanDir);
 
@@ -30,31 +35,31 @@ for i = 1 : length(type)
     NF = length(zSorted);
     for j = 1 : NF
         reconPath = fullfile(masterDir, 'Stack', char(type(i)), sprintf('%0.2f', zSorted(j)));
-        timePath = dir(reconPath);
-        Ntimes = length(timePath(not([timePath.isdir])));
+        % timePath = dir(reconPath);
+        % Ntimes = length(timePath(not([timePath.isdir])));
         % Ntimes is the number of times in the reconstruction slice
-        times = 0:Ntimes-1;
+        times = 0:length(dupes)-1;
         % remove duplicate from times
-        times(dupes) = [];
+        times(logical(dupes)) = [];
         % Calculate the number of batches to be processed
-        batches = floor(length(times)/(batchSize));
+        batches = floor(length(times)/(bSize));
         switch GPU
             case 'Yes'
                 if batches==0
                     I = gpuArray(zeros(N(1), N(2), length(times)));
                 else
-                    I = gpuArray(zeros(N(1), N(2), batchSize));
+                    I = gpuArray(zeros(N(1), N(2), bSize));
                 end
             case 'No'
                 if batches==0
                     I = zeros(N(1), N(2), length(times));
                 else
-                    I = zeros(N(1), N(2), batchSize);
+                    I = zeros(N(1), N(2), bSize);
                 end  
         end
         for k = 0 : batches - 1
-            for ii = 1 : batchSize
-                I(:, :, ii) = imread(fullfile(reconPath, sprintf('%05d.tiff', k*batchSize+ii-1)));
+            parfor ii = 1 : bSize
+                I(:, :, ii) = imread(fullfile(reconPath, sprintf('%05d.tiff', times(k*bSize+ii))));
             end
             I_mean = meanSubtraction(I);
             dataDir = fullfile(meanDir, char(type(i)), sprintf('%0.2f', zSorted(j)));
@@ -65,13 +70,13 @@ for i = 1 : length(type)
                     parfor kk = 1 : size(I_mean, 3)
                         I_temp = freqFilterGPU(I_mean(:,:,kk).*255, mask, centerX, centerY, N);
                         I_temp = gather(I_temp);
-                        imwrite(I_temp, fullfile(dataDir, sprintf('%05d.tiff', times(kk))))
+                        imwrite(I_temp, fullfile(dataDir, sprintf('%05d.tiff', times(k*bSize+kk))))
                     end
                 case 'No'
                     parfor kk = 1 : size(I_mean, 3)
                         I_temp = freqFilter(I_mean(:,:,kk).*255, mask, centerX, centerY, N);
                         I_mean(:,:,kk) = I_temp;
-                        imwrite(I_mean(:,:,kk), fullfile(dataDir, sprintf('%05d.tiff', times(kk))))
+                        imwrite(I_mean(:,:,kk), fullfile(dataDir, sprintf('%05d.tiff', times(k*bSize+kk))))
                     end
             end
         end
@@ -79,12 +84,12 @@ for i = 1 : length(type)
         % Process remaining files
         switch GPU
             case 'Yes'
-                I = gpuArray(zeros(N(1), N(2), length(times)-batches*batchSize));
+                I = gpuArray(zeros(N(1), N(2), length(times)-batches*bSize));
             case 'No'
-                I = zeros(N(1), N(2), length(times)-batches*batchSize);
+                I = zeros(N(1), N(2), length(times)-batches*bSize);
         end
-        for jj = 1 : (length(times)-batches*fps*t)
-            I(:, :, jj) = imread(fullfile(reconPath, sprintf('%05d.tiff', batches*batchSize+jj-1)));
+        parfor jj = 1 : (length(times)-batches*bSize)
+            I(:, :, jj) = imread(fullfile(reconPath, sprintf('%05d.tiff', times(batches*bSize+jj))));
         end
         I_mean = meanSubtraction(I);
         dataDir = fullfile(meanDir, char(type(i)), sprintf('%0.2f', zSorted(j)));
@@ -95,16 +100,17 @@ for i = 1 : length(type)
                 parfor kk = 1 : size(I,3)
                     I_temp = freqFilterGPU(I_mean(:,:,kk).*255, mask, centerX, centerY, N);
                     I_temp = gather(I_temp);
-                    imwrite(I_temp, fullfile(dataDir, sprintf('%05d.tiff', times(kk))))
+                    imwrite(I_temp, fullfile(dataDir, sprintf('%05d.tiff', times(batches*bSize+kk))))
                 end
             case 'No'
                 parfor kk = 1 : size(I,3)
                     I_temp = freqFilter(I_mean(:,:,kk).*255, mask, centerX, centerY, N);
                     I_mean(:,:,kk) = I_temp;
-                    imwrite(I_mean(:,:,kk), fullfile(dataDir, sprintf('%05d.tiff', times(kk))))
+                    imwrite(I_mean(:,:,kk), fullfile(dataDir, sprintf('%05d.tiff', times(batches*bSize+kk))))
                 end
         end
     end
 end
 % Save metaData before ending function
 save(fullfile(meanDir, 'metaData.mat'), 'times', 'zSorted')
+toc
